5e75394f37cecf13325e87ac0e1ed542b327c6f6
NikitaAsmedianov <nikitaa@campus.technion.ac.il>
2023-02-19 17:15:17 +0200

    Have to read paragraph to the end. Everething works

--- main.py
+++ main.py
@@ -3,13 +3,15 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import numpy as np
+import matplotlib.pyplot as plt
 
-N = 4
+N = 1000
 h = 1.0 / N
 i_ = np.arange(N)
 x_ = i_ * h
-E = 150.0e3 * np.ones(N)  # Pa
+E = 150.0e3 * np.exp(-x_*x_*10)  # Pa
 G = 9.8 * 1.0e3 * 1.0 * np.ones(N)  # Pa
+G[len(G)//2] = 2.0e3 * G[0]
 
 
 def phi_(i: int, x):
@@ -41,14 +43,19 @@ def F_(i: int):
 
 K_f = 0
 for i in i_:
+    matrix = \
+        np.dot(K_(i), S_(i))
     K_f += \
-        S_(i).transpose().dot(K_(i).dot(S_(i)))
+        np.dot(S_(i).transpose(), matrix)
 F_f = 0
 for i in i_:
     F_f += \
         S_(i).transpose().dot(F_(i))
-d=np.linalg.det(K_f)
-U_f = np.linalg.inv(K_f).dot(F_f)
-pass
+K_f = K_f[1:].transpose()[1:].transpose()
+F_f = F_f[:-1]
+K_f_inv = np.linalg.inv(K_f)
+U_f = np.dot(K_f_inv, F_f)
+plt.plot(U_f)
+plt.show()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
